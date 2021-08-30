a8e5d2888177cd1877d9c05c0a7fc9e607aa80b3
Xenovka <wendyanto18@gmail.com>
2021-08-30 19:53:55 +0700

    makes input only accept numbers

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,6 +1,26 @@
 <template>
-  <input type="text" id="the-input" class="calc-input bg-darker" />
+  <input
+    @input="inputValidation"
+    type="text"
+    id="the-input"
+    class="calc-input bg-darker"
+  />
 </template>
+
+<script>
+export default {
+  methods: {
+    inputValidation(event) {
+      let inputValue = event.target.value;
+      const inputElement = document.getElementById("the-input");
+
+      const nonNumber = /([^a-zA-Z\D]){1,}/g;
+      const invalidInput = inputValue.match(nonNumber);
+      inputElement.value = invalidInput.join("");
+    }
+  }
+};
+</script>
 
 <style scoped>
 @import "../assets/css/Input.css";
